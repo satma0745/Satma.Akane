c04f02210f1c20a16b788f7273260dc8580f6b4c
Bogdan Shahnitsky <satma0745@gmail.com>
2024-01-08 23:27:38 +0300

    Add Open API Specification.

--- Satma.Akane.HttpApi/Program.cs
+++ Satma.Akane.HttpApi/Program.cs
@@ -1,9 +1,26 @@
+using Microsoft.OpenApi.Models;
+
 var webApplicationBuilder = WebApplication.CreateBuilder(args);
 
+webApplicationBuilder.Services.AddSwaggerGen(options =>
+{
+    const string apiVersion = "v1";
+
+    var openApiInfo = new OpenApiInfo
+    {
+        Title = "Satma.Akane",
+        Version = apiVersion
+    };
+    
+    options.SwaggerDoc(apiVersion, openApiInfo);
+});
 webApplicationBuilder.Services.AddControllers();
 
 var webApplication = webApplicationBuilder.Build();
 
 webApplication.MapControllers();
+
+webApplication.UseSwagger();
+webApplication.UseSwaggerUI();
 
 webApplication.Run();
