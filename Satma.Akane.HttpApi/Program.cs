ed5884a8b2af33cc8682d99cd4cf3ca32c9b3806
Bogdan Shahnitsky <satma0745@gmail.com>
2024-01-08 23:37:20 +0300

    Put Swagger Registration under a distinct Namespace.

--- Satma.Akane.HttpApi/Program.cs
+++ Satma.Akane.HttpApi/Program.cs
@@ -1,26 +1,13 @@
-using Microsoft.OpenApi.Models;
+using Satma.Akane.HttpApi.Swagger;
 
 var webApplicationBuilder = WebApplication.CreateBuilder(args);
 
-webApplicationBuilder.Services.AddSwaggerGen(options =>
-{
-    const string apiVersion = "v1";
-
-    var openApiInfo = new OpenApiInfo
-    {
-        Title = "Satma.Akane",
-        Version = apiVersion
-    };
-    
-    options.SwaggerDoc(apiVersion, openApiInfo);
-});
+webApplicationBuilder.Services.AddSwaggerSpecification();
 webApplicationBuilder.Services.AddControllers();
 
 var webApplication = webApplicationBuilder.Build();
 
 webApplication.MapControllers();
-
-webApplication.UseSwagger();
-webApplication.UseSwaggerUI();
+webApplication.UseSwaggerSpecification();
 
 webApplication.Run();
---
+++ Satma.Akane.HttpApi/Swagger/Registration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Satma.Akane.HttpApi.Swagger;
+
+internal static class Registration
+{
+    private const string apiVersion = "v1";
+
+    private static readonly OpenApiInfo openApiInfo = new()
+    {
+        Title = "Satma.Akane",
+        Version = apiVersion
+    };
+    
+    public static void AddSwaggerSpecification(this IServiceCollection services) =>
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc(apiVersion, openApiInfo);
+        });
+
+    public static void UseSwaggerSpecification(this WebApplication webApplication)
+    {
+        webApplication.UseSwagger();
+        webApplication.UseSwaggerUI();
+    }
+}
